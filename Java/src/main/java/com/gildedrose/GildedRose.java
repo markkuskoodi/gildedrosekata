f394b6c8eaea4fabf87ce1fc8b7d162edaca098c
markkuskoodi <markkus999@outlook.com>
2021-04-10 00:10:04 +0300

    Paar testi ning mingi lambise loogika vea parandasin ära

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -24,13 +24,13 @@ class GildedRose {
             else if(itemName.contains("conjured"))
                 items[i] = updateConjuredItem(items[i]);
             else{
-                if((items[i].quality > 50 || items[i].quality < 0) && items[i].quality != 80){
+                if((items[i].quality > 50 || items[i].quality < 0) && items[i].quality != 80)
                     throw new UnsupportedOperationException("Quality of the item is no go");
-                } else if(itemName.contains("sulfuras"))
+                else if(itemName.contains("sulfuras"))
                     continue;
-                else {
+                else
                     items[i] = updateRegularItem(items[i]);
-                }
+
             }
         }
     }
@@ -49,13 +49,12 @@ class GildedRose {
         if(item.sellIn == -1){
             item.quality = 0;
             return item;
-        } else if(item.sellIn > 5 && item.sellIn <= 10){
+        } else if(item.sellIn > 5 && item.sellIn <= 10)
             item.quality += 2;
-        } else if(item.sellIn <= 5 && item.sellIn >= 0){
+        else if(item.sellIn <= 5 && item.sellIn >= 0)
             item.quality += 3;
-        } else {
+        else
             item.quality += 1;
-        }
         item.sellIn -= 1;
         return item;
     }
@@ -64,16 +63,19 @@ class GildedRose {
         if(item.sellIn == -1){
             item.quality /= 4;
         } else{
-            item.quality -= 2;
+            if(item.quality >= 2)
+                item.quality -= 2;
+            else if(item.quality != 0)
+                item.quality -= 1;
             item.sellIn -= 1;
         }
         return item;
     }
 
     private Item updateRegularItem(Item item){
-        if(item.sellIn == -1){
+        if(item.sellIn == -1)
             item.quality /= 2;
-        } else {
+        else {
             item.quality -= 1;
             item.sellIn -= 1;
         }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -13,5 +13,56 @@ class GildedRoseTest {
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
     }
-    
+
+    @Test
+    void updateAgedBrie(){
+        Item[] items = new Item[] { new Item("Aged Brie", 5, 0) };
+        GildedRose app = new GildedRose(items);
+
+        for (int i = 0; i < 5; i++) {
+            app.updateQuality();
+        }
+        assertEquals(5, app.items[0].quality);
+
+        app.updateQuality();
+        app.updateQuality();
+
+        assertEquals(3, app.items[0].quality);
+    }
+
+    @Test
+    void updateBackstage(){
+        Item[] items = new Item[] { new Item("Backstage passes", 10, 0) };
+        GildedRose app = new GildedRose(items);
+
+        for (int i = 0; i < 5; i++) {
+            app.updateQuality();
+        }
+        assertEquals(10, app.items[0].quality);
+
+        for (int i = 0; i < 6; i++) {
+            app.updateQuality();
+        }
+
+        assertEquals(28, app.items[0].quality);
+
+        app.updateQuality();
+
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void updateConjured(){
+        Item[] items = new Item[] { new Item("Conjured item", 5, 16), new Item("Conjured 2", 5, 10) };
+        GildedRose app = new GildedRose(items);
+
+        for (int i = 0; i < 7; i++) {
+            app.updateQuality();
+            System.out.println(app.items[0].quality + " " + app.items[0].sellIn);
+        }
+        assertEquals(1, app.items[0].quality);
+        assertEquals(0, app.items[1].quality);
+    }
+
+
 }
